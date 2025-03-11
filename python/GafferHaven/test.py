4ebc0585ad03d2220060be5bd860a78d533014ea
patricesos <patricesos7@gmail.com>
2025-03-11 09:59:28 +0000

    Update test.py

--- python/GafferHaven/test.py
+++ python/GafferHaven/test.py
@@ -4,34 +4,8 @@ from utils import haven_api_query
 # import importlib
 # importlib.reload(GafferHaven)
 
-<<<<<<< Updated upstream
-def havenApiQuery(arguments):
-	user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-
-	url = "https://api.polyhaven.com/"+arguments
-	headers={'User-Agent':user_agent,} 
-
-	request=urllib.request.Request(url,None,headers) #The assembled request
-	response = urllib.request.urlopen(request)
-	data = response.read() # The data u need
-	#print(data)
-	jdata = json.loads(data)
-	print(type(jdata))
-	for h in jdata.keys():
-		print(jdata[h])
-
-
-#print(havenApiQuery("assets?t=hdris"))
-#print(havenApiQuery("files/abandoned_church"))
-#print(havenApiQuery("info/muddy_autumn_forest"))
-#print(havenApiQuery("categories/hdris"))
-
-import havenAssetLibrary
-l = havenAssetLibrary.library()
-=======
 
 # print(haven_api_query("assets?t=hdris"))
 # print(haven_api_query("files/abandoned_church"))
 # print(haven_api_query("info/muddy_autumn_forest"))
 print(haven_api_query("categories/hdris"))
->>>>>>> Stashed changes

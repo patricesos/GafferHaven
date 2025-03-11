13e122782fdd899cae0fde3e678a3d663504ad11
patricesos <patricesos7@gmail.com>
2025-03-11 09:59:23 +0000

    Update appendBrowseButtons.py

--- startup/gui/appendBrowseButtons.py
+++ startup/gui/appendBrowseButtons.py
@@ -1,52 +1,33 @@
+import os
+
+import Gaffer
+import GafferCycles
 import GafferHaven
-import Gaffer, GafferUI, os, imath
-import GafferCycles
+import GafferUI
+import imath
 
-
-import GafferCycles
 Gaffer.Metadata.registerValue(
-	GafferCycles.CyclesShader,
-	"layout:activator:textureIsEnv", lambda node : node["name"].getValue() == "environment_texture"
-) 
-Gaffer.Metadata.registerValue(
-	GafferCycles.CyclesShader, "layout:customWidget:browseHDRI:widgetType", "GafferHaven.gui.BrowseButton",
+    GafferCycles.CyclesShader,
+    "layout:activator:textureIsEnv",
+    lambda node: node["name"].getValue() == "environment_texture",
 )
+
 Gaffer.Metadata.registerValue(
-<<<<<<< Updated upstream
-	GafferCycles.CyclesShader, "layout:customWidget:browseHDRI:section", "Settings",
-=======
     GafferCycles.CyclesShader,
     "layout:customWidget:browseHDRI:widgetType",
     "GafferHaven.guiTest.BrowseButton",
->>>>>>> Stashed changes
 )
+
 Gaffer.Metadata.registerValue(
-	GafferCycles.CyclesShader,
-	"layout:customWidget:browseHDRI:visibilityActivator", "textureIsEnv"
+    GafferCycles.CyclesShader,
+    "layout:customWidget:browseHDRI:visibilityActivator",
+    "textureIsEnv",
 )
 
 
 if "ARNOLD_ROOT" in os.environ:
-	import GafferArnold
-	Gaffer.Metadata.registerValue(
-		GafferArnold.ArnoldShader,
-		"layout:activator:shaderIsImage", lambda node : node["name"].getValue() == "image"
-	) 
-	Gaffer.Metadata.registerValue(
-		GafferArnold.ArnoldShader, "layout:customWidget:browseHDRI:widgetType", "GafferHaven.gui.BrowseButton",
-	)
-	Gaffer.Metadata.registerValue(
-		GafferArnold.ArnoldShader, "layout:customWidget:browseHDRI:section", "Settings",
-	)
-	Gaffer.Metadata.registerValue(
-		GafferArnold.ArnoldShader,
-		"layout:customWidget:browseHDRI:visibilityActivator", "shaderIsImage"
-	)
+    import GafferArnold
 
-
-
-<<<<<<< Updated upstream
-=======
     Gaffer.Metadata.registerValue(
         GafferArnold.ArnoldShader,
         "layout:activator:shaderIsImage",
@@ -67,4 +48,3 @@ if "ARNOLD_ROOT" in os.environ:
         "layout:customWidget:browseHDRI:visibilityActivator",
         "shaderIsImage",
     )
->>>>>>> Stashed changes
